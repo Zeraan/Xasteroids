ee75889dae640355ce6270b95de027b621bc9db3
jwwilcken <jwwilcken@xactware.com>
2014-01-31 15:07:12 -0700

    shorter interval for asteroid pushes

--- GameMain.cs
+++ GameMain.cs
@@ -53,7 +53,7 @@ namespace Xasteroids
 		private Host _host;
 		private Client _client;
 		private Timer _200msPushDataTimer = new Timer { Interval = 100 };
-		private Timer _5000msPushDataTimer = new Timer { Interval = 5000 };
+		private Timer _5000msPushDataTimer = new Timer { Interval = 2000 };
 		public ShipSelectionWindow ShipSelectionWindow { get; private set; }
 		
 		public object ChatLock = new object();
